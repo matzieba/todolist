80d1b7b1c1b4456076738fcf518a831569236730
mati <matizieba@gmail.com>
2022-04-07 14:50:28 +0200

    exception handlig + footer

--- todolist/users/views.py
+++ todolist/users/views.py
@@ -8,20 +8,22 @@ from django.contrib import messages
 def register(request):
     form = RegisterForm()
 
-
     if request.method == 'POST':
+        messages.add_message(request, messages.INFO, 'This username is already taken. Please choose another one!')
         form = RegisterForm(request.POST)
         if form.is_valid():
-
             # Create user and save to the database
-            user = User.objects.create_user(
-                form.cleaned_data["your_name"],
-                form.cleaned_data["your_email"],
-                form.cleaned_data["your_password"],
-            )
+            try:
+                user = User.objects.create_user(
+                    form.cleaned_data["your_name"],
+                    form.cleaned_data["your_email"],
+                    form.cleaned_data["your_password"],
+                )
             # user.username =
             # user.email =
             # user.password =
-            user.save()
+                user.save()
+            except:
+                return redirect("register")
         return redirect("welcome")
     return render(request, "register.html", {"form":form})
