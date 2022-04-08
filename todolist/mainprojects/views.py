424758d1d9306d28fe29a66ff07539bcd4906930
mati <matizieba@gmail.com>
2022-04-08 14:48:49 +0200

    Relativ DB + views

--- todolist/mainprojects/views.py
+++ todolist/mainprojects/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import MainProject
 from .forms import ProjectForm
+from tasklist.models import Task
 
 
 @login_required
@@ -21,7 +22,8 @@ def new(request):
 @login_required
 def detail(request, id):
     proj = get_object_or_404(MainProject, pk=id)
-    return render(request, "mainprojects/detail.html", {"proj": proj})
+    tasks = Task.objects.filter(main_project__id=id)
+    return render(request, "mainprojects/detail.html", {"proj": proj, "tasks":tasks})
 
 
 from django.shortcuts import render
