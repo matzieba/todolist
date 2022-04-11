5f8e84b4a236a0b89833dd0bb026bab83368ba88
Mateusz Zieba <matizieba@gmail.com>
2022-04-11 18:52:32 +0200

    heroku deploy

--- todolist/todolist/wsgi.py
+++ todolist/todolist/wsgi.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 
 import os
 import sys
+sys.path.append('C:/Users/Mati/Desktop/dev/protfolio 100doc/todolist')
+sys.path.append('C:/Users/Mati/Desktop/dev/protfolio 100doc/todolist/todolist')
 
 from django.core.wsgi import get_wsgi_application
-sys.path.append('C:\Users\Mati\Desktop\dev\protfolio 100doc\todolist')
-sys.path.append('C:\Users\Mati\Desktop\dev\protfolio 100doc\todolist\todolist')
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todolist.settings')
 
 application = get_wsgi_application()
