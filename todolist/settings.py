1d8edc7cfda98a9045878724e3d53aa8b68be065
Mateusz Zieba <matizieba@gmail.com>
2022-04-11 20:02:05 +0200

    heroku deploy

--- todolist/settings.py
+++ todolist/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['pixeltodo.herokuapp.com']
 
@@ -124,7 +124,7 @@ USE_TZ = True
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/static/website'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
