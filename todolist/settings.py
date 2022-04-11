cb95045415df35fc893898ab00ee7991f15a6259
Mateusz Zieba <matizieba@gmail.com>
2022-04-11 19:14:50 +0200

    heroku deploy

--- todolist/settings.py
+++ todolist/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'www.mysite.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
