9292ddeabcf62c2e7b081899e1af9dfb29fcefcb
mati <matizieba@gmail.com>
2022-04-12 09:45:08 +0200

    repair

--- todolist/settings.py
+++ todolist/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'asfagfadgsadghdfhgfdh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['todomixer.herokuapp.com']
 
 
 # Application definition
