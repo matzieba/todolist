310660c5a622aec43314c656a39681aceb327db5
mati <matizieba@gmail.com>
2022-04-12 10:25:00 +0200

    repair

--- todolist/settings.py
+++ todolist/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import mimetypes
-
-mimetypes.add_type("text/css", ".css", True)
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,8 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'staticfiles/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static/"),
 )
