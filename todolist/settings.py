6846dc354387595c192dbbf4fc6607c5f39d1601
mati <matizieba@gmail.com>
2022-04-12 09:49:35 +0200

    repair

--- todolist/settings.py
+++ todolist/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Default primary key field type
