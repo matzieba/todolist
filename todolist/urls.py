7ea34cfa1c428b0f145870dee30e1d9fdceaffce
mati <matizieba@gmail.com>
2022-04-12 11:02:57 +0200

    repair

--- todolist/urls.py
+++ todolist/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users.views import register
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +28,4 @@ urlpatterns = [
     path('register', register, name = 'register'),
     path('mainprojects/', include('mainprojects.urls'))
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
